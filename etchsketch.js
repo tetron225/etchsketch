d29392a6a7202ddccd0ca1ecfdfe79931dd9bfc6
tetron225 <skim6502@gmail.com>
2024-08-11 17:13:49 -0400

    fixed so that the divs look like grids

--- etchsketch.js
+++ etchsketch.js
@@ -2,10 +2,11 @@
 
 //calling the container div and adding it to the container variable
 let container = document.querySelector("#container");
+container.setAttribute("style", "width: 500px; height: 500px; display: flex; flex-direction: row; flex-wrap: wrap;")
 //using a loop to make the 16x16 div boxes
 for(let i = 1; i < 17; i++) {
     let boxdiv = document.createElement("div");
-    boxdiv.setAttribute("style", "background-color: gray; padding: 10px; margin: 10px;");
+    boxdiv.setAttribute("style", "background-color: gray; padding: 50px; margin: 1px;");
     container.appendChild(boxdiv);
 }
 
