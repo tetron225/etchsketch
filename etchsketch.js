38f3ecd5e1b66eb1e5738c465a10e01eec9afb9d
tetron225 <skim6502@gmail.com>
2024-08-12 00:02:01 -0400

    adjusted how the grid works and it should fit into some form of a quadrilateral

--- etchsketch.js
+++ etchsketch.js
@@ -2,11 +2,11 @@
 
 //calling the container div and adding it to the container variable
 let container = document.querySelector("#container");
-container.setAttribute("style", "width: 1000px; height: 1000px; display: flex; flex-direction: row; flex-wrap: wrap;")
+container.setAttribute("style", "width: auto; height: 500 px; display: flex; flex-direction: row; flex-wrap: wrap; align-items: center; justify-content:center;")
 //using a loop to make the 16x16 div boxes
 for(let i = 1; i <= 256; i++) {
     let boxdiv = document.createElement("div");
-    boxdiv.setAttribute("style", "background-color: gray; padding: 30px; margin: 0px;");
+    boxdiv.setAttribute("style", "background-color: gray; padding: 30px; margin: 0px; flex-basis: 1%; flex-grow: 1;");
     boxdiv.addEventListener("mouseover", () => {
         boxdiv.style.backgroundColor = "blue";
     })
@@ -18,12 +18,15 @@ let input = document.querySelector("#number");
 
 button.addEventListener("click", () => {
     let num = input.value;
+    if(num > 100) {
+
+    }
     while(container.firstChild) {
         container.removeChild(container.firstChild);
     }
     for(let i = 1; i <= num * num; i++) {
         let boxdiv = document.createElement("div");
-        boxdiv.setAttribute("style", "background-color: gray; padding: 30px; margin: 0px;");
+        boxdiv.setAttribute("style", "background-color: gray; padding: 30px; margin: 0px; flex-basis: 1%; flex-grow: 1;");
         boxdiv.addEventListener("mouseover", () => {
             boxdiv.style.backgroundColor = "blue";
         })
